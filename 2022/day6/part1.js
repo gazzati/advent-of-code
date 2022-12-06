6e72987db834ef59dd3ea8be6c6eafa44406aae1
t.gazzaev <t.gazzaev@sftpro.ru>
2022-12-06 11:11:53 +0300

    Day 6

---
+++ 2022/day6/part1.js
@@ -0,0 +1,15 @@
+const data = document.getElementsByTagName('pre')[0].textContent.split("\n")[0]
+
+let result = 0
+
+for(let i = 3; i < data.length; i++) {
+    const subStr = data.slice(i - 3, i + 1)
+
+    if(!subStr.split('').every((item, index, arr) => arr.indexOf(item) === index)) continue
+
+    result = i + 1
+    break
+}
+
+
+console.log(result)
