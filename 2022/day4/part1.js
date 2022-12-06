cba479db5ed10f295c7b17c14661139fcbf8fe8b
t.gazzaev <t.gazzaev@sftpro.ru>
2022-12-06 11:10:52 +0300

    Day 4

---
+++ 2022/day4/part1.js
@@ -0,0 +1,19 @@
+const data = document.getElementsByTagName('pre')[0].textContent.slice(0, -1).split('\n')
+
+const generateArray = (str) => {
+    const [start, stop] = str.split("-")
+    return Array.from({ length: (stop - start) + 1}, (_, i) => Number(start) + i)
+}
+
+const result = data.reduce((acc, item) => {
+    const [first,second] = item.split(",")
+
+    const firstArr = generateArray(first)
+    const secondArr = generateArray(second)
+
+    if(firstArr.every(digit=> secondArr.includes(digit)) || secondArr.every(digit=> firstArr.includes(digit))) return acc + 1
+
+    return acc
+}, 0)
+
+console.log(result)
